7910683d6b251692e5fc1ccd18c7f4e46c093aaf
filipkrzyz <krzyzanowski.f@gmail.com>
2019-12-25 20:12:15 +0100

    Initial Commit

---
+++ ClassfiedsMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClassfiedsMVVM
+//
+//  Created by Filip Krzyzanowski on 25/12/2019.
+//  Copyright © 2019 Filip Krzyzanowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
